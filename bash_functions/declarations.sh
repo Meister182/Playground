467b6dabf01d9c8e627ef80ffc20cb8f9baf8fc8
Alexandre Marques <alexandre.marques@ctw.bmwgroup.com>
2022-06-06 18:11:49 +0100

    bash: function declaration order
    - experimenting if a function can call another that is declared after.
    - experimenting if a function could be called before being declared.

--- bash_functions/declarations.sh
+++ bash_functions/declarations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Experimenting with the syntax and white spaces when declaring functions.
+## Experimenting with the syntax and white spaces when declaring functions.
 # *personal favorite.
 function my_print_1(){
     echo "this is function 1."
@@ -17,7 +17,24 @@ my_print_3(){
     echo "this is function 3."
 }
 
-
 my_print_1
 my_print_2
 my_print_3
+
+
+## Experimenting with the declaration order.
+# can a function call another one called after it?
+# *Yes, as long as it is called after it is declred
+function first_function(){
+    echo "I'm first!"
+    second_function
+}
+
+# Throws an error if called here: " line 25: second_function: command not found"
+# first_function
+
+function second_function(){
+    echo "I'm second!"
+}
+
+first_function
