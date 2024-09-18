f49a90e2b2a08822856e99dd59bbdf5f3448c973
Alexander Marques <c137.marques@gmail.com>
2024-09-18 17:59:40 +0100

    packaging_setuptools: Add a flag argument to my custom package

--- python/packaging_setuptools/my_py_pkg/__main__.py
+++ python/packaging_setuptools/my_py_pkg/__main__.py
@@ -1,9 +1,30 @@
 #! /usr/bin/env python3
+"""
+My custom setuptools python package
+"""
+import argparse
+
 from my_py_pkg.mods.foo import print_foo
 
-def main():
-    print("My python package!")
-    print_foo()
+def arg_parser(argv=None):
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        "--foo",
+        action="store_true",
+        help="Print foo message."
+    )
+
+    return parser.parse_args(argv)
+
+
+def main(argv=None):
+    args = arg_parser(argv)
+
+    print("Running my setuptools python package!")
+
+    if args.foo:
+        print_foo()
 
 if __name__ == "__main__":
     main()
--- python/packaging_setuptools/run.py
+++ python/packaging_setuptools/run.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
+from sys import argv
 from my_py_pkg.__main__ import main
 
 if __name__ == "__main__":
-    main()
+    main(argv[1:])
